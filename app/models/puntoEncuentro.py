4e74f992ec836c0e8b27335bc001f14db30d9bc7
Mercedes Scattini <mechi1999@hotmail.com>
2021-10-10 11:28:39 -0300

    issues #21 y #22, faltan terminar

--- app/models/puntoEncuentro.py
+++ app/models/puntoEncuentro.py
@@ -1,6 +1,5 @@
 
-from sqlalchemy import Column, Integer, String, Text, ForeignKey
-from geoalchemy2 import Geometry
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, validates
 
 from app.db import db
@@ -11,9 +10,9 @@ class PuntoEncuentro(db.Model):
 
     __tablename__ = "PuntosDeEncuentro"
     id = Column(Integer, primary_key=True)
-    nombre = Column(String(40), unique=True)
-    direccion = Column(Text, nullable=False)
-    coordenadas = Column(String(3)) 
+    nombre = Column(String(40), unique=True, nullable=False)
+    direccion = Column(String(30), unique=True, nullable=False)
+    coordenadas = Column(String(80)) 
     estado_id = Column(Integer, ForeignKey("statuses.id"))
     estado = relationship(Status)
     telefono = Column(String(30))
@@ -29,18 +28,24 @@ class PuntoEncuentro(db.Model):
     
     @validates('direccion')
     def validate_direccion(self, key, direccion):
+        """Chequea que se ingrese una dirección"""
+
         if not direccion:
             raise ValueError("Debe ingresar una direccion")
         return direccion
 
     @validates('nombre')
     def validate_nombre(self, key, nombre):
+        """Chequea que se ingrese un nombre"""
+
         if not nombre:
             raise ValueError("Debe ingresar un nombre")
         return nombre
 
     @validates('email')
     def validate_email(self, key, email):
+        """Chequea que se ingrese un email válido"""
+
         if email and '@' not in email:
             raise ValueError("Ingrese un mail válido")
         return email
--- app/resources/puntoEncuentro.py
+++ app/resources/puntoEncuentro.py
@@ -1,4 +1,5 @@
 from flask import redirect, render_template, request, url_for, session, abort, flash
+from sqlalchemy import exc
 
 from app.models.puntoEncuentro import PuntoEncuentro
 from app.helpers.auth import authenticated, check_permission
@@ -11,12 +12,12 @@ def index():
 
     user = User.query.filter(User.email==session['user'])
 
-    """ if not check_permission(user[0].id, 'punto_encuentro_index'):
-        abort(401) """
+    if not check_permission(user[0].id, 'punto_encuentro_index'):
+        abort(401) 
     
     #falta módulo de configuración para traer la cant de elementos por pagina
     #cantPaginas = Config.query....
-    cantPaginas = 2 #puse 2 de prueba
+    cantPaginas = 4 #puse 2 de prueba
 
     page = request.args.get('page', 1, type=int)
     puntos = PuntoEncuentro.query.paginate(page=page, per_page=cantPaginas)
@@ -29,8 +30,8 @@ def new():
 
     user = User.query.filter(User.email==session['user'])
 
-    """ if not check_permission(user[0].id, 'punto_encuentro_new'):
-        abort(401) """
+    if not check_permission(user[0].id, 'punto_encuentro_new'):
+        abort(401)
     
     return render_template("puntoEncuentro/new.html")
 
@@ -40,8 +41,8 @@ def create():
 
     user = User.query.filter(User.email==session['user'])
 
-    """ if not check_permission(user[0].id, 'punto_encuentro_create'):
-        abort(401)  """
+    if not check_permission(user[0].id, 'punto_encuentro_create'):
+        abort(401) 
 
     #catchea todos los errores que levantan los validadores de campos
     try:
@@ -52,12 +53,66 @@ def create():
     else:
         db.session.add(new_punto)
 
-    #si el nombre ingresado ya se encuentra registrado en la db se produce la exeption
+    #si el nombre ingresado o direccion ya se encuentra registrado en la db se produce la exeption
     try:
         db.session.commit()
-    except:
-        flash("Ya existe un punto de encuentro con ese nombre")
+    except exc.IntegrityError as e:
+        if 'direccion' in e.orig.args[1]:
+            flash("Ya existe un punto con esa direccion")
+        elif 'nombre' in e.orig.args[1]:
+            flash("Ya existe un punto con ese nombre")
         return render_template("puntoEncuentro/new.html")
-        
 
     return redirect(url_for("puntoEncuentro_index"))
+
+def update(id_punto):
+    if not authenticated(session):
+        abort(401)
+
+    user = User.query.filter(User.email==session['user'])
+
+    if not check_permission(user[0].id, 'punto_encuentro_create'):
+        abort(401) 
+
+    punto = PuntoEncuentro.query.get_or_404(id_punto)
+    if request.method == 'POST':
+        
+        punto.coordenadas = request.form['coordenadas']
+        punto.estado_id = request.form['estado_id']
+        punto.telefono = request.form['telefono']
+
+        try:
+            punto.nombre = request.form['nombre']
+            punto.direccion = request.form['direccion']
+            punto.email = request.form['email']
+            db.session.commit()
+        except exc.IntegrityError as e: #maneja las excepciones de datos ya ingresados en db
+            if 'direccion' in e.orig.args[1]:
+                flash("Ya existe un punto con esa direccion")
+            elif 'nombre' in e.orig.args[1]:
+                flash("Ya existe un punto con ese nombre")
+            db.session.rollback()
+            return render_template("puntoEncuentro/edit.html", puntoEncuentro=punto) 
+        except ValueError as e: #maneja la validación de los campos
+            flash(e)
+            db.session.rollback()
+            return render_template("puntoEncuentro/edit.html", puntoEncuentro=punto)
+        flash("Punto actualizado")
+        return redirect(url_for("puntoEncuentro_index")) 
+    return render_template('puntoEncuentro/edit.html', puntoEncuentro=punto)
+
+def destroy(id_punto):
+    if not authenticated(session):
+        abort(401)
+
+    user = User.query.filter(User.email==session['user'])
+
+    if not check_permission(user[0].id, 'punto_encuentro_create'):
+        abort(401)  
+    
+    punto = PuntoEncuentro.query.get_or_404(id_punto)
+    db.session.delete(punto)
+    db.session.commit()
+    
+    return redirect(url_for("puntoEncuentro_index"))
+

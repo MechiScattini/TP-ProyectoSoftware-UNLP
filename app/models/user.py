cdd52424837f937215f1c54204b88441d567095e
Recalde iñaki <iniakirecalde@hotmail.com>
2021-10-11 01:45:07 -0300

    intentando crear tabla intermedia users_roles (push para ayuda)

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,16 @@
 from enum import unique
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import backref
+from sqlalchemy.orm import backref, relationship
 from sqlalchemy.sql.expression import true
 from sqlalchemy.sql.sqltypes import Boolean
 from app.db import db
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Table, Column, Integer, ForeignKey
 
-
+Base = declarative_base()
+users_roles= Table ('users_roles', Base.metadata,
+    Column('user_id' ,db.Integer, ForeignKey('users.id'),primary_key=True),
+    Column('rol_id', db.Integer, ForeignKey('roles.id'),primary_key=True) )
 class User(db.Model):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
@@ -14,7 +19,8 @@ class User(db.Model):
     email = Column(String(30), unique=True)
     password = Column(String(30))
     bloqueado = Column(Boolean, default= False)
-    username = Column(String,unique = True)
+    username = Column(String(39),unique = True)
+    roles = relationship( "Rol", secondary=users_roles, back_populates="users")
     
     def __init__(self, username=None,first_name=None, last_name=None, email=None, password=None):
         self.first_name = first_name
@@ -30,6 +36,9 @@ class Permiso(db.Model):
     name = Column(String(30), unique=True)
 
 class Rol(db.Model):
-    __tablename__ = "roles"
+    __tablename__ = 'roles'
     id = Column(Integer, primary_key=True)
     name = Column(String(30), unique=True)
+    roles = relationship( "User", secondary=users_roles, back_populates="roles")
+
+  

32e02c457ccb441e97038f238e94c73bf8127ff3
Mercedes Scattini <mechi1999@hotmail.com>
2021-11-24 20:02:16 -0300

    home page+componente zonas

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-4">
+
+      <div class="conteiner-info">
+      <h2>Todo sobre denuncias</h2>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+      <a class="nav-link" href="#">Link</a>
+      </div>
+
+      <div class="conteiner-info">
+      <h3 class="mt-4">Links de interes</h3>
+      <p>Lorem ipsum dolor sit ame.</p>
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+      <hr class="d-sm-none">
+      </div>
+    </div>
+    <div class="col-sm-8">
+      <div class="conteiner-info">
+      <h2>Puntos de Encuentro</h2>
+      <div class="fakeimg">Fake Image</div>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <a class="nav-link active" href="#">Ver mas</a>
+      </div>
+      <div class="conteiner-info">
+      <h2 class="mt-5">Zonas inundables</h2>
+      <p>Información y visualización de las zonas inundables de La Plata.</p>
+      <router-link to="/zonas-inundables" class="nav-link">Ver mas</router-link>
+      </div>
+      <div class="conteiner-info">
+      <h2 class="mt-5">Recorridos de evacuacion</h2>
+      <div class="fakeimg">Fake Image</div>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <a class="nav-link active" href="#">Ver mas</a>
+      </div>
+    </div>
   </div>
+</div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>

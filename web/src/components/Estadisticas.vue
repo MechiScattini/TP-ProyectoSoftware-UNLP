c3bd130febbf6f71208c96e5bd3282a25f7c3793
Mercedes Scattini <mechi1999@hotmail.com>
2021-12-04 13:55:07 -0300

    sintaxis arreglada y console.log --

--- web/src/components/Estadisticas.vue
+++ web/src/components/Estadisticas.vue
@@ -1,27 +1,24 @@
 <template>
-    <div>
-        <h2>Estadisticas</h2>
-        <h2>{{estadisticas.cantidad_puntos}}</h2>
-    </div>
+  <div>
+    <h2>Estadisticas</h2>
+    <h2>{{estadisticas.cantidad_puntos}}</h2>
+  </div>
 </template>
 <script>
 export default {
-    data () {
-        return {
-            estadisticas: {}
-        }
-    },
-    async created () {
-        try {
-            const response = await fetch('https://admin-grupo18.proyecto2021.linti.unlp.edu.ar/api/estadisticas/')
-            console.log(response)
-            const json = await response.json()
-            this.estadisticas = json.estadisticas
-            console.log(this.estadisticas)
-        }     
-        catch (e) {
-            alert(e)
-        }
+  data () {
+    return {
+      estadisticas: {}
+    }
+  },
+  async created () {
+    try {
+      const response = await fetch('https://admin-grupo18.proyecto2021.linti.unlp.edu.ar/api/estadisticas/')
+      const json = await response.json()
+      this.estadisticas = json.estadisticas
+    } catch (e) {
+      alert(e)
     }
   }
+}
 </script>
